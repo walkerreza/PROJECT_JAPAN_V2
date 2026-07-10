8fcd60e9adc39570731ecdd9b37eadce0a2ab564
anka reza <rezawalker02@gmail.com>
2026-07-10 10:27:27 +0700

    feat: initialize japanlingo v2

---
+++ routes/console.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Schedule::command('subscriptions:expire')->dailyAt('00:10');
+Schedule::command('presentations:cleanup-imports --days=14')->dailyAt('00:30');
